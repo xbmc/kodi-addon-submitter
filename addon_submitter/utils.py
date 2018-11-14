7c4c453708dc4e3acf03f45c0673d7877b882c29
Rechi <Rechi@users.noreply.github.com>
2018-11-14 16:33:08 +0100

    remove redundant quotes in user.name

--- addon_submitter/utils.py
+++ addon_submitter/utils.py
@@ -129,7 +129,7 @@ def create_addon_branch(work_dir, repo, branch, addon_id, version):
         shutil.rmtree(repo_dir)
     shell('git', 'clone', repo_fork)
     os.chdir(repo)
-    shell('git', 'config', 'user.name', '"{}"'.format(gh_username))
+    shell('git', 'config', 'user.name', '{}'.format(gh_username))
     shell('git', 'config', 'user.email', email)
     shell('git', 'remote', 'add', 'upstream',
           'https://github.com/xbmc/{}.git'.format(repo))
